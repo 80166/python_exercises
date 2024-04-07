0af8c20a9d0897fe35ec49dfb5640f89356ac99d
80166 <pritamburnwal.100@gmail.com>
2024-04-07 11:42:17 +0530

    34th exercise completed

---
+++ Ex_34.py
@@ -0,0 +1,9 @@
+animals = ["bear", "python", "peacock", "kangaroo", "whale", "platypus"]
+print("The animal is at 1 index 1 and is a:      %s  " % animals[1])
+print("Third animal is at index 2 and is a:      %s  " % animals[2])
+print("The first animal is at index 0 and is a:  %s " % animals[0])
+print("The animal at 3 is at index 3 and is a:   %s " % animals[3])
+print("The fifth animal is at index 5 and is a:  %s " % animals[5])
+print("The animal at 2 is at index 2 and is a:   %s " % animals[2])
+print("The sixth animal is at index 5 and is a:  %s  " % animals[5])
+print("The animal at 4 is at index 4 and is a:   %s " % animals[4])
